3b76af63138b6a51766c8cf32a8b1ec2aa0f74be
Yonatan Verhskov <yav2002>
2020-11-30 16:06:10 +0200

    update

--- main.py
+++ main.py
@@ -10,15 +10,10 @@ def df_void(t, x):
     return v_x, v_y, 0, -9.81
 
 
-def df_grar(t, x):
-    rho = 1.184
-    m = 35
-    c = 0.4
-    g = -9.81
-    A = math.pi * 0.001125
+def df_grar(t, x, rho=1.184, m=35, c=0.4, g=-9.81, a=math.pi * 0.001125):
     x, y, v_x, v_y = x
 
-    mekadem = (c * A * rho * math.sqrt(v_x ** 2 + v_y ** 2)) / m
+    mekadem = (c * a * rho * math.sqrt(v_x ** 2 + v_y ** 2)) / m
     new_v_x = -mekadem * v_x
     new_v_y = g - mekadem * v_x
     return v_x, v_y, new_v_x, new_v_y
@@ -29,7 +24,7 @@ def deg_2_rad(deg):
 
 
 def rocket_void(v_0, angle, save_file=True):
-    t = np.linspace(0, 50, 1000)
+    t = np.linspace(0, 50, 2000)
     start_params = [0, 0, v_0 * math.cos(deg_2_rad(angle)), v_0 * math.sin(deg_2_rad(angle))]
     r = sp.odeint(df_void, start_params, t, tfirst=True)
     r = np.ndarray.transpose(r)
@@ -50,10 +45,15 @@ def rocket_void(v_0, angle, save_file=True):
     return r
 
 
-def rocket_with_grar(v_0, angle, save_file=True):
-    t = np.linspace(0, 50, 1000)
+def simulate_hit(v_0, angle):
+    r = rocket_with_grar(v_0, angle, save_file=False)
+    return r[0][-1]
+
+
+def rocket_with_grar(v_0, angle, save_file=True, rho=1.184, m=35, c=0.4, g=-9.81, a=math.pi * 0.001125):
+    t = np.linspace(0, 50, 2000)
     start_params = [0, 0, v_0 * math.cos(deg_2_rad(angle)), v_0 * math.sin(deg_2_rad(angle))]
-    r = sp.odeint(df_grar, start_params, t, tfirst=True)
+    r = sp.odeint(df_grar, start_params, t, tfirst=True, args=(rho,m,c,g,a))
     r = np.ndarray.transpose(r)
 
     t_star = np.argmin([abs(a) for a in r[1][1:]])
@@ -72,6 +72,28 @@ def rocket_with_grar(v_0, angle, save_file=True):
     return r
 
 
+def aim(dest):
+    angel1 = 1
+    angel2 = 2
+    v_0 = 225
+    while abs(angel2-angel1)>0.01:
+        temp_angel_1 = angel1
+        angel1 = angel2
+        angel2 = angel2 - simulate_hit(v_0, angel2) * (angel2 - temp_angel_1) / (simulate_hit(v_0, angel2) - simulate_hit(v_0, temp_angel_1))
+    return angel2
+
+
+def min_dis(ux, uy, vx, vy):
+    dis_lst = []
+    for i in range(len(ux)):
+        dis_lst.append((ux[i] - vx[i]) ** 2 + (uy[i] - vy[i]) ** 2)
+    min = dis_lst[0]
+    for i in range(len(dis_lst)):
+        if dis_lst[i] <= min:
+            min = dis_lst[i]
+    return min
+
+
 if __name__ == "__main__":
     # rocket_void(225,50)
-    rocket_with_grar(225, 50)
+    print(aim(4000))
