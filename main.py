05af982ddc8f7e2fbbe0e790e68609d8c3ee67d7
t9004165 <gadotshay>
2020-11-30 16:23:18 +0200

    minimal dis

--- main.py
+++ main.py
@@ -46,13 +46,13 @@ def rocket_void(v_0, angle, save_file=True):
 
 
 def simulate_hit(v_0, angle):
-    r = rocket_with_grar(v_0, angle, save_file=False)
+    r = rocket_with_grar(0, v_0, angle, save_file=False)
     return r[0][-1]
 
 
-def rocket_with_grar(v_0, angle, save_file=True, rho=1.184, m=35, c=0.4, g=-9.81, a=math.pi * 0.001125):
+def rocket_with_grar(x_0, v_0, angle, save_file=True, rho=1.184, m=35, c=0.4, g=-9.81, a=math.pi * 0.001125):
     t = np.linspace(0, 50, 2000)
-    start_params = [0, 0, v_0 * math.cos(deg_2_rad(angle)), v_0 * math.sin(deg_2_rad(angle))]
+    start_params = [x_0, 0, v_0 * math.cos(deg_2_rad(angle)), v_0 * math.sin(deg_2_rad(angle))]
     r = sp.odeint(df_grar, start_params, t, tfirst=True, args=(rho,m,c,g,a))
     r = np.ndarray.transpose(r)
 
@@ -96,4 +96,5 @@ def min_dis(ux, uy, vx, vy):
 
 if __name__ == "__main__":
     # rocket_void(225,50)
-    print(aim(4000))
+    #print(aim(4000))
+    pass
--- minimal distance.py
+++ minimal distance.py
@@ -11,8 +11,11 @@ def min_dis(ux, uy, vx, vy):
     return min
 
 
-def min_distance_kassam ( theta )
-    kassam_x = rocket_void(0, 225, 50, save_file=True)[0]
-    kassam_y = rocket_void(0, 225, 50, save_file=True)[1]
-    kipa_x = rocket_void(0.75 * kassam_x[-1], 225, theta, save_file=True)[0]
-    kipa_y = rocket_void(0.75 * kassam_x[-1], 225, theta, save_file=True)[1]
+def min_distance_kassam ( theta ):
+    kassam_x = rocket_with_grar(0, 225, 50, save_file=False)[0]
+    kassam_y = rocket_with_grar(0, 225, 50, save_file=False)[1]
+    kipa_x = rocket_with_grar(0.75 * kassam_x[-1], 225, theta, save_file=False)[0]
+    kipa_y = rocket_with_grar(0.75 * kassam_x[-1], 225, theta, save_file=False, c = 0.28)[1]
+    return math.sqrt(min_dis(kassam_x, kassam_y, kipa_x, kipa_y))
+
+
